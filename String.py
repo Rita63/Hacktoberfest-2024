d30dd3d968634576b1103925c554a6bc511c6580
Rita Bindal <42319102+Rita63@users.noreply.github.com>
2024-10-16 23:05:16 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,18 @@
+# initializing string 
+test_str = "Geeks"
+
+# initializing number
+test_int = 4
+
+# printing original string 
+print("The original string is : " + test_str)
+
+# printing number
+print("The original number : " + str(test_int))
+
+# using type conversion
+# Inserting number in string 
+res = test_str + str(test_int) + test_str
+
+# printing result 
+print("The string after adding number is : " + str(res))
